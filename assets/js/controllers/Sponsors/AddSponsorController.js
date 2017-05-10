c537c29110b97572caa45cdc4e03bd216d953dad
Serene <debukali@gmail.com>
2017-05-10 16:30:42 +0300

    Add sponsor without profile image

--- assets/js/controllers/Sponsors/AddSponsorController.js
+++ assets/js/controllers/Sponsors/AddSponsorController.js
@@ -9,9 +9,33 @@ kacheApp.controller('AddSponsor', function(
 	){
 	var vm = $scope;
 	vm.payload = {};
-	vm.addSponsor = function(file){
-		vm.payload.file = file;
-		vm.payload.thumb = "thumb";
+	vm.addSponsor = function(event, file){
+		var addButton = $(event.currentTarget);
+		if(vm.payload.txtuname && vm.payload.txtemail && vm.payload.txtpass){
+			addButton.button('loading');
+			if(file){
+				vm.payload.file = file;
+				vm.payload.thumb = "thumb";
+				uploadFile(file, addButton);
+			}else {
+				vm.payload.sponsor = "sponsor";
+				var link = 'http://imagevibez.com/church/signup.php';
+				uploadText(link, vm.payload, addButton);
+			}
+			
+		}else {
+			utilityService.notify('All fields required', 'info');
+		}
+	}
+
+	function uploadText(url, data, btn){
+		httpService.post(url, data).then(function(response){
+			console.log("Success "+JSON.stringify(response));
+			clearForm(btn)
+		});
+	}
+
+	function uploadFile(file, btn){
 		file.upload = Upload.upload({
 			url: 'http://imagevibez.com/church/sponsorSignup.php',
 			data: vm.payload
@@ -19,51 +43,25 @@ kacheApp.controller('AddSponsor', function(
 
 		file.upload.then(function (response) {
 			$timeout(function () {
+				clearForm(addButton);
 				file.result = response.data;
 				console.log("Success "+JSON.stringify(response.data));
+				utilityService.notify('Sponsor Account Created', 'success');
 			});
 		}, function (response) {
+			btn.button('reset');
 			if (response.status > 0)
 				$scope.errorMsg = response.status + ': ' + response.data;
-		}, function (evt) {
-	      // Math.min is to fix IE which reports 200% sometimes
-	      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-	    });
-		
-		/*
-		var addButton = $(event.currentTarget);
-
-		if(vm.payload.uname && vm.payload.txtemail && vm.payload.txtpass){
-			addButton.button('loading');
-			var link = 'http://imagevibez.com/church/postSponsor.php';
-			httpService.post(link, vm.payload).then(function(response){
-				if(response.data.success == 1){
-					console.log("Add sponsor success ");
-					utilityService.notify('Added Sponsor', 'success');
-				}else{
-					console.log("Add sponsor fail "+response.data.message);
-				}
-				
-				clearForm();
-
-				addButton.button('reset');
-			}, function(error){
-				clearForm();
-
-				console.log("Add sponsor Error "+JSON.stringify(error));
-				addButton.button('reset');
-
-				utilityService.notify('Cannot Add Sponsor', 'danger');
-			});
-		}else {
-			utilityService.notify('All fields required', 'info');
-		}*/
+			utilityService.notify('Something went wrong, send us an email if it persists '+response.data, 'info');
+		});
 	}
 
-	function clearForm(){
-		vm.payload.uname = "";
+	function clearForm(btn){
+		vm.payload.txtuname = "";
 		vm.payload.txtemail = "";
 		vm.payload.txtpass = "";
+		vm.picFile = !vm.picFile;
+		btn.button('reset');
 	}
 
 });
